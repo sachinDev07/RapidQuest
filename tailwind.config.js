b446a96aa0f8bcd4f584610aeab317373e702980
sachinDev07 <sachinkrj077@gmail.com>
2024-01-09 10:54:15 +0530

    setup colors and font and installed react-router-dom

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#4935ff',
+        secondary: '#000000',
+        bgSlate: '#fafafa',
+        mdSlate: '#646464',
+      },
+      fontFamily: {
+        heading: ['Lora', 'serif'],
+      },
+    },
   },
   plugins: [],
 };
